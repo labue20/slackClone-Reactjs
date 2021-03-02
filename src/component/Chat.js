7c5aa26ebf50026152b09fc42ce075396344b861
labue20 <labuewilfred@gmail.com>
2021-03-01 22:31:39 -0600

    another commit

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,18 +1,39 @@
 
 import React from 'react'
 import styled from 'styled-components'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import { InfoOutlined, InfoRounded } from '@material-ui/icons';
+import ChatInput  from './ChatInput';
+import ChatMessage from './ChatMessage';
 
 function Chat() {
     return (
         <Container>
             <Header>
-                
+                  
+                <Channel>
+                    <ChannelName>
+                    # Willz
+                       
+                    </ChannelName>
+                    <ChannelInfor>
+                            Company-wde announcement
+                       </ChannelInfor>
+
+                </Channel>
+                <ChannelDetails>
+                    <div>
+                        Details
+                    </div>
+                    <Info/>
+                    
+                </ChannelDetails>
             </Header>
 
             <MessageContainer>
-                    
+                    <ChatMessage/>
             </MessageContainer>
-            <ChatInput></ChatInput>
+            <ChatInput/>
            
         </Container>
     )
@@ -26,10 +47,36 @@ const Container = styled.div`
     background
 `
 const Header = styled.div`
-    background: orange;
+    padding-left:20px;
+    padding-right:20px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid rgba(83,39,83,.13);
+    justify-content: space-between;
 `
 const MessageContainer = styled.div`
-    background: gray;
+    
 `
-const ChatInput = styled.div`
+
+
+const Channel  = styled.div`
+
+`
+const Info = styled(InfoOutlinedIcon)`
+    margin-left:10px;
+`
+const ChannelDetails = styled.div`
+    display:flex;
+    align-items: center;
+    color: #606060;
+  
+`
+const ChannelInfor = styled.div`
+    font-weight = 400;
+    color: #606060;
+    font-size: 13px;
+    margin-top: 8px;
+`
+const ChannelName = styled.div`
+    font-weight = 700;
 `
